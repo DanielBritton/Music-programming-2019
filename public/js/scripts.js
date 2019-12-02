57561121a5d6716607c8f8a6dcf6a8f8bc060db8
DanielBritton <danielsbritton@gmail.com>
2019-12-02 23:48:51 +0000

    Added background images to all files, added a save function and coded JS for displaying songs and artists on search page

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -19,6 +19,23 @@ function openPage(pageName, elmnt, color) {
     elmnt.style.backgroundColor = color;
 }
 
+function saveSong(row) {
+    if (localStorage.getItem('library') === null) {
+        var storage = [row];
+        localStorage.setItem('library', JSON.stringify(storage))
+        console.log('Creating new library...')
+        console.log(storage)
+    } else {
+        var library = JSON.parse(localStorage.getItem('library'))
+        library.push(row)
+        localStorage.setItem('library', JSON.stringify(library));
+    }
+}
+
+function loadSongs() {
+    return JSON.parse(localStorage.library)
+}
+
 function validateSearch() {
     inputVal = document.getElementById("inputVal").value
     console.log(inputVal)
@@ -26,26 +43,98 @@ function validateSearch() {
         makeVisible('error')
     } else {
         makeInvisible('error')
-        data = searchByTrackName(inputVal);
+        // document.getElementById('trackResults').innerHTML += ("<p>Songs like " + inputVal + "</p>")
+        // document.getElementById('artistResults').innerHTML += ("<p>Artists like " + inputVal +"</p>")
 
-        document.getElementById('basicResults').innerHTML = data
-        document.getElementById('searchResults').style.visibility = 'visible'
+        document.getElementById('trackHeader').innerHTML = ("<p>Songs like " + "\"" + inputVal + "\"" + "</p>")
+        document.getElementById('artistHeader').innerHTML = ("<p>Artists like " + "\"" + inputVal + "\"" + "</p>")
+
+        searchByTrackName(inputVal)
+        searchByArtistName(inputVal);
     }
 }
 
-function searchByTrackName(query) {
-    request = ("https://api.musixmatch.com/ws/1.1/track.search?format=jsonp&q_track=" + query + "&apikey=802224f7ee4779fbc74a6ebaf2347221")
-    //callback=jsonpcallback& //before q_track
-    // $.ajax({
-    //     type: "GET", url: request,
-    //     success: function (data) {
-    //         console.log(data)
-    //         return data;
-    //     }
-    // })
-    fetch(request).then(data => {
-        console.log(data.json())
+function searchByArtistName(query) {
+    var divName = "artistResults"
+    $.ajax({
+        type: "GET",
+        data: {
+            apikey: "802224f7ee4779fbc74a6ebaf2347221",
+            q_artist: query,
+            s_track_rating: "DESC",
+            s_artist_rating: "DESC",
+            format: "jsonp",
+            callback: "jsonp_callback"
+        },
+        url: "https://api.musixmatch.com/ws/1.1/track.search",
+        dataType: "jsonp",
+        jsonpCallback: 'jsonp_callback',
+        success: function (data) {
+            data = data.message.body.track_list
+            serveResultsTable(data, divName)
+            document.getElementById('searchResults').style.visibility = 'visible'
+        }
     })
+}
+
+function searchByTrackName(query) {
+    var divName = "trackResults"
+    $.ajax({
+        type: "GET",
+        data: {
+            apikey: "802224f7ee4779fbc74a6ebaf2347221",
+            q_track: query,
+            s_track_rating: "DESC",
+            format: "jsonp",
+            callback: "jsonp_callback"
+        },
+        url: "https://api.musixmatch.com/ws/1.1/track.search",
+        dataType: "jsonp",
+        jsonpCallback: 'jsonp_callback',
+        success: function (data) {
+            data = data.message.body.track_list
+            serveResultsTable(data, divName)
+            document.getElementById('searchResults').style.visibility = 'visible'
+        }
+    })
+}
+
+function generalQuery(query) {
+    var divName = ""
+    $.ajax({
+        type: "GET",
+        data: {
+            apikey: "802224f7ee4779fbc74a6ebaf2347221",
+            q: query,
+            s_track_rating: "DESC",
+            s_artist_rating: "DESC",
+            format: "jsonp",
+            callback: "jsonp_callback"
+        },
+        url: "https://api.musixmatch.com/ws/1.1/track.search",
+        dataType: "jsonp",
+        jsonpCallback: 'jsonp_callback',
+        success: function (data) {
+            data = data.message.body.track_list
+            serveResultsTable(data, divName)
+            document.getElementById('searchResults').style.visibility = 'visible'
+        }
+    })
+}
+
+function serveResultsTable(data, divName) {
+    console.log(data)
+    var table = "<table><tr> <th>Song</th> <th>Artist</th> <th>Album</th> <th>Save</th> </tr>"
+    for (i = 0; i < data.length; i++) {
+        table +=
+            "<tr>" + "<td>" + data[i].track.track_name + "</td>" +
+            "<td>" + data[i].track.artist_name + "</td>" +
+            "<td>" + data[i].track.album_name + "</td>" +
+            "<td>" + "<button class = 'add'>" + "+" + "</button>" + "</td>";
+    };
+    table += "</table>";
+    console.log(table)
+    document.getElementById(divName).innerHTML = table
 }
 
 
