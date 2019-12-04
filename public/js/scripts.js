441dfa182c7a69dd80881b7b19bbbdd093f61f45
DanielBritton <danielsbritton@gmail.com>
2019-12-04 12:35:54 +0000

    Completed library page and delete functionality

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -17,6 +17,9 @@ function openPage(pageName, elmnt, color) {
 
     // Add the specific color to the button used to open the tab content
     elmnt.style.backgroundColor = color;
+    if (pageName == 'Library') {
+        displayLibrary()
+    }
 }
 
 function saveSong(row) {
@@ -24,7 +27,6 @@ function saveSong(row) {
         var storage = [row];
         localStorage.setItem('library', JSON.stringify(storage))
         console.log('Creating new library...')
-        console.log(storage)
     } else {
         var library = JSON.parse(localStorage.getItem('library'))
         library.push(row)
@@ -32,13 +34,34 @@ function saveSong(row) {
     }
 }
 
+function deleteSong(index) {
+    var local = loadSongs()
+    local.splice(index - 1, 1);
+    localStorage.setItem('library', JSON.stringify(local))
+    console.log("Item Deleted: " + localStorage.getItem('library'))
+    displayLibrary()
+}
+
+function displayLibrary() {
+    var header = "<tr><th>Song</th><th>Artist</th><th>Album</th><th>Delete</th></tr>"
+    var local = loadSongs().join('')
+    if (local === null || local == []) {
+        document.getElementById('displayLib').innerHTML = "<br/><br/><h4>You haven't saved any songs yet...</h4><br/><br/>"
+    } else {
+        document.getElementById('displayLib').innerHTML = header + local
+        deleteButtons = document.getElementsByClassName('deleteCell')
+        for (i = 0; i < deleteButtons.length; i++) {
+            deleteButtons[i].innerHTML = "<button class = 'deleteButton' onclick = 'deleteSong(this.parentNode.parentNode.rowIndex)'>X</button>"
+        }
+    }
+}
+
 function loadSongs() {
-    return JSON.parse(localStorage.library)
+    return JSON.parse(localStorage.getItem('library'))
 }
 
 function validateSearch() {
     inputVal = document.getElementById("inputVal").value
-    console.log(inputVal)
     if (inputVal.length == 0) {
         makeVisible('error')
     } else {
@@ -84,7 +107,8 @@ function searchByTrackName(query) {
         data: {
             apikey: "802224f7ee4779fbc74a6ebaf2347221",
             q_track: query,
-            s_track_rating: "DESC",
+            s_artist_rating: "DESC",
+            //s_track_rating: "DESC",
             format: "jsonp",
             callback: "jsonp_callback"
         },
@@ -123,20 +147,29 @@ function generalQuery(query) {
 }
 
 function serveResultsTable(data, divName) {
-    console.log(data)
     var table = "<table><tr> <th>Song</th> <th>Artist</th> <th>Album</th> <th>Save</th> </tr>"
     for (i = 0; i < data.length; i++) {
+
         table +=
-            "<tr>" + "<td>" + data[i].track.track_name + "</td>" +
+            "<tr " + "id=" + divName + i + ">" + "<td>" + data[i].track.track_name + "</td>" +
             "<td>" + data[i].track.artist_name + "</td>" +
             "<td>" + data[i].track.album_name + "</td>" +
-            "<td>" + "<button class = 'add'>" + "+" + "</button>" + "</td>";
+            "<td>" + "<button class = 'add' id = b_" + divName + i + " onclick = addToLibrary(this.parentNode.parentNode,this.id)" + ">" + "+" + "</button>" + "</td>";
     };
     table += "</table>";
     console.log(table)
     document.getElementById(divName).innerHTML = table
 }
 
+function addToLibrary(song, id) {
+    song.removeAttribute('id')
+    var action = document.getElementById(id).parentElement
+    action.className = 'deleteCell'
+    document.getElementById(id).remove()
+    saveSong(song.outerHTML)
+    action.removeAttribute('class')
+    console.log("Adding Song: " + song.outerHTML)
+}
 
 function makeVisible(elmnt) {
     document.getElementById(elmnt).style.visibility = 'visible'
@@ -150,4 +183,3 @@ function makeInvisible(elmnt) {
 window.addEventListener('load', function () {
     document.getElementById("defaultOpen").click();
 })
-
